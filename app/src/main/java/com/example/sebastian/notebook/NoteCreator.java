d169fb92de7ea8660ac18a3f707044a9ff0029a1
ssaleta <sebastiansaleta@gmail.com>
2017-02-09 17:40:51 +0100

    create note creator activity

---
+++ app/src/main/java/com/example/sebastian/notebook/NoteCreator.java
@@ -0,0 +1,13 @@
+package com.example.sebastian.notebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteCreator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_creator);
+    }
+}
